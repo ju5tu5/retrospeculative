9113f29d9c98d71c27813b46acc16f12da067067
ju5tu5 <justus@ju5tu5.nl>
2018-04-23 13:47:51 +0200

    GA!

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,5 +2,6 @@ module.exports = {
   title: "Retrospeculative",
   description: "It's a collection of retrospeculative science fiction gems become fact.",
   dest: "docs",
+  ga: "UA-117927119-1",
 
 }
